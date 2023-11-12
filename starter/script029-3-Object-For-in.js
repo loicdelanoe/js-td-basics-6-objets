4b28626080d898fbb8be370dc7363c043b1ff0d2
Loïc Delanoë <loic.del4127@gmail.com>
2023-11-12 17:11:02 +0100

    Finished script029-3-Object-For-in.js

--- starter/script029-3-Object-For-in.js
+++ starter/script029-3-Object-For-in.js
@@ -15,31 +15,39 @@ EXERCICE 1 :
 Parcours l'objet mesFruits pour afficher, pour chacun des fruits, "Le fruit … coûte … €"
 */
 const mesFruits = {
-  "Pomme": 1.3,
-  "Poire": 0.5,
-  "Ananas": 3
+    "Pomme": 1.3,
+    "Poire": 0.5,
+    "Ananas": 3
 };
 
 // Écris ton code ici
+for (const mesFruitsKey in mesFruits) {
+    console.log(`Le fruit ${mesFruitsKey} coûte ${mesFruits[mesFruitsKey]}€`);
+}
 
 /*
 EXERCICE 2 :
 Parcours l'objet fruit pour afficher chacune de ses propriétés avec, pour chacune, sa valeur
 */
 const fruit = {
-  "Nom": "Pomme",
-  "Prix": 0.3,
-  "Couleur": "Verte",
-  "Variété": "Golden"
+    "Nom": "Pomme",
+    "Prix": 0.3,
+    "Couleur": "Verte",
+    "Variété": "Golden"
 };
 
 // Écris ton code ici
-
+for (const fruitKey in fruit) {
+    console.log(`${fruitKey} : ${fruit[fruitKey]}`);
+}
 // Un mot est un objet de type String => ceci fonctionne également…
 /*
 EXERCICE 3 :
 Parcours le mot "Bonjour" pour afficher séparément chacune de ses lettres
 */
-const mot = "Bonjour";
 
 // Écris ton code ici
+const mot = "Bonjour";
+for (const motKey in mot) {
+    console.log(mot[motKey]);
+}
